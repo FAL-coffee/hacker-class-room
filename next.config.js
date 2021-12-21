9649410707c70b45fc2998a550d6d5c1c433c868
neighbor.net <neighbor.net.official@gmail.com>
2021-12-21 22:46:08 +0900

    Update：nextconf, tsconf, storybook/mainに、components、types、src/用のエイリアスを設定

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+const path = require('path')
 module.exports = {
   reactStrictMode: true,
+  webpack(config) {
+    config.resolve.alias = {
+      "@": path.resolve(__dirname, "src/"),
+      "@types": path.resolve(__dirname, "src/types"),
+      "@components": path.resolve(__dirname, "src/components"),
+    };
+    return config;
+  },
 }
