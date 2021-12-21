4b5e550e5ad8ac5b75af8a115baeead6536e1678
neighbor.net <neighbor.net.official@gmail.com>
2021-12-21 22:31:17 +0900

    Add：storybookの設定ファイルの設置

---
+++ .storybook/main.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../src/components/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/components/**/stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-actions",
+    "@storybook/addon-essentials",
+  ]
+}
