e6f4f01497169a6c0c7d910e5ff97b30f9d59a0b
neighbor.net <neighbor.net.official@gmail.com>
2021-12-30 16:29:37 +0900

    Fix：既存のaliasを削除・上書きする記述であったため、修正を実行

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
   reactStrictMode: true,
   webpack(config) {
     config.resolve.alias = {
+      ...config.resolve.alias,
       "@": path.resolve(__dirname, "src/"),
       "@types": path.resolve(__dirname, "src/types"),
       "@components": path.resolve(__dirname, "src/components"),
