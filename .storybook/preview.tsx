f7a6f6bb6bce9816835884e1dd565630f32e3990
neighbor.net <neighbor.net.official@gmail.com>
2022-01-04 18:27:55 +0900

    Add：previewの作成 mui対応まで

---
+++ .storybook/preview.tsx
@@ -0,0 +1,20 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import { StylesProvider } from "@mui/styles";
+import theme from "../src/styles/theme";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+};
+
+const withThemeProvider = (Story, context) => {
+  return (
+    <StylesProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Story {...context} />
+      </ThemeProvider>
+    </StylesProvider>
+  );
+};
+export const decorators = [withThemeProvider];
