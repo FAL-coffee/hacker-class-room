b3cf0163743b0ea250f03c6174f081a53b36fef2
DAIKO\fukudomeh <fukudomeh@daikoiws.co.jp>
2022-01-12 15:05:44 +0900

    Update：logo・リンクのクリックイベントを記述

--- src/components/organisms/AppHeader/AppHeader.tsx
+++ src/components/organisms/AppHeader/AppHeader.tsx
@@ -14,22 +14,29 @@ import MenuItem from "@mui/material/MenuItem";
 
 import { Props } from "./types";
 
-const pages = ["Products", "Pricing", "Blog"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
+import Image from "next/image";
+
+// const pages = ["Products", "Pricing", "Blog"];
+const links = [{ key: "products", name: "Products" }];
+// const settings = ["Profile", "Account", "Dashboard", "Logout"];
+const userMenu = [
+  { key: "profile", name: "Profile" },
+  { key: "logout", name: "Logout" },
+];
 
 export const AppHeader = ({ ...props }: Props) => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
 
-  const handleOpenNavMenu = (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    // setAnchorElNav(event.currentTarget);
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
   };
-  const handleOpenUserMenu = (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    // setAnchorElUser(event.currentTarget);
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
   };
 
   const handleCloseNavMenu = () => {
@@ -43,16 +50,15 @@ export const AppHeader = ({ ...props }: Props) => {
     <AppBar position="fixed">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+          <Button
+            size="large"
+            sx={{ display: { xs: "none", md: "flex" } }}
+            onClick={props.onLogoClick}
           >
-            LOGO
-          </Typography>
+            <Image src="/logo.png" alt="alt" height={50} width={140} />
+          </Button>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+          <Box sx={{ flexGrow: 0, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -81,29 +87,39 @@ export const AppHeader = ({ ...props }: Props) => {
                 display: { xs: "block", md: "none" },
               }}
             >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
+              {links.map((link) => (
+                <MenuItem
+                  key={link.key}
+                  onClick={() => (
+                    handleCloseNavMenu(), props.onLinkClick(link.key)
+                  )}
+                >
+                  <Typography textAlign="center">{link.name}</Typography>
                 </MenuItem>
               ))}
             </Menu>
           </Box>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+          <Box
+            sx={{
+              flexGrow: 2,
+              display: { xs: "flex", md: "none" },
+            }}
+            textAlign="justify"
           >
-            LOGO
-          </Typography>
+            <Button size="large" onClick={props.onLogoClick}>
+              <Image src="/logo.png" alt="alt" height={50} width={140} />
+            </Button>
+          </Box>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
+            {links.map((link) => (
               <Button
-                key={page}
-                onClick={handleCloseNavMenu}
+                key={link.key}
+                onClick={() => (
+                  handleCloseNavMenu(), props.onLinkClick(link.key)
+                )}
                 sx={{ my: 2, color: "white", display: "block" }}
               >
-                {page}
+                {link.name}
               </Button>
             ))}
           </Box>
@@ -130,9 +146,14 @@ export const AppHeader = ({ ...props }: Props) => {
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
+              {userMenu.map((item) => (
+                <MenuItem
+                  key={item.key}
+                  onClick={() => (
+                    handleCloseUserMenu(), props.onUserMenuClick(item.key)
+                  )}
+                >
+                  <Typography textAlign="center">{item.name}</Typography>
                 </MenuItem>
               ))}
             </Menu>
--- src/components/organisms/AppHeader/types.ts
+++ src/components/organisms/AppHeader/types.ts
@@ -1,4 +1,7 @@
 import { IUser } from "@types";
 export interface Props {
   user: IUser;
+  onLogoClick: () => void;
+  onUserMenuClick: (key: string) => void;
+  onLinkClick: (key: string) => void;
 }
