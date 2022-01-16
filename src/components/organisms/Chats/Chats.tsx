52a41124c1e2935802d5043875329ee62a47af00
neighbor.net <neighbor.net.official@gmail.com>
2022-01-16 17:29:12 +0900

    Add：テストの追加

--- src/components/organisms/Chats/Chats.tsx
+++ src/components/organisms/Chats/Chats.tsx
@@ -2,6 +2,9 @@ import { Box } from "@mui/material";
 import { Message } from "@/components/molecules";
 import { Props, UserMessage } from "./types";
 export const Chats = ({ ...props }: Props) => {
+  const isMine = (userMessage: UserMessage): boolean => {
+    return userMessage.user.uid === props.user?.uid;
+  };
   return (
     <>
       {props.userMessageList?.map((userMessage: UserMessage, i) => (
@@ -9,15 +12,14 @@ export const Chats = ({ ...props }: Props) => {
           key={i}
           sx={{
             mb: 2,
-            textAlign:
-              userMessage.user.uid === props.user?.uid ? "right" : "left",
+            textAlign: isMine(userMessage) ? "right" : "left",
           }}
         >
           <Box sx={{ display: "inline-flex" }}>
             <Message
               user={userMessage.user}
               message={userMessage.message}
-              isMine={userMessage.user.uid === props.user?.uid}
+              isMine={isMine(userMessage)}
               onIconClick={() => undefined}
             />
           </Box>
