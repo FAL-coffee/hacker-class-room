583cd5d66209ebfa376cae5d8b31f326b9ff53a1
neighbor.net <neighbor.net.official@gmail.com>
2022-01-17 17:54:27 +0900

    Add：画面表示時とmessageListの更新時に最下にscrollする処理を追加

--- src/components/organisms/Chats/Chats.tsx
+++ src/components/organisms/Chats/Chats.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Box } from "@mui/material";
 import { Message } from "@/components/molecules";
 import { Props, UserMessage } from "./types";
@@ -5,8 +6,20 @@ export const Chats = ({ ...props }: Props) => {
   const isMine = (userMessage: UserMessage): boolean => {
     return userMessage.user.uid === props.user?.uid;
   };
+
+  const ref = React.createRef<HTMLDivElement>();
+  const scrollToButtom = React.useCallback(() => {
+    ref!.current!.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+    });
+  }, [ref]);
+
+  React.useEffect(() => {
+    scrollToButtom();
+  }, [props.userMessageList]);
   return (
-    <>
+    <Box ref={ref}>
       {props.userMessageList?.map((userMessage: UserMessage, i) => (
         <Box
           key={i}
@@ -25,6 +38,6 @@ export const Chats = ({ ...props }: Props) => {
           </Box>
         </Box>
       ))}
-    </>
+    </Box>
   );
 };
