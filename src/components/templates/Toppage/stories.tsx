c9eebbf31ca368df3e47ea1ba606712a8095f1f8
neighbor.net <neighbor.net.official@gmail.com>
2022-01-20 13:55:28 +0900

    remove：waste import

--- src/components/templates/Toppage/stories.tsx
+++ src/components/templates/Toppage/stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
+// import { action } from "@storybook/addon-actions";
 import { Toppage } from ".";
 
 storiesOf("@components/template/Toppage", module)
