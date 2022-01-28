55e85e696806953b09c9e7ce8682943929a6421b
neighbor.net <neighbor.net.official@gmail.com>
2022-01-28 18:18:10 +0900

    Add：共通テストデータの作成

---
+++ src/fixtures/ChatRoom.ts
@@ -0,0 +1,15 @@
+import { IChatRoom, IMessage } from "@types";
+import { USER } from "./User";
+
+export const CHATROOM: IChatRoom = {
+  id: "test",
+  owner: USER,
+  name: "test chatrooms name",
+  iconURL: "/favicon.ico",
+  tags: [
+    { id: "test-tag1-id", value: "test-tag1-value" },
+    { id: "test-tag2-id", value: "test-tag2-value" },
+  ],
+  description: "hello! this is chatroom card testing for localhost.",
+  createdAt: { seconds: 0, nanoseconds: 0 } as IMessage["postedAt"],
+};
