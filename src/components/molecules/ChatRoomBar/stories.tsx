581aae810b5f9754dcb55681d9fd5937d8551fb0
neighbor.net <neighbor.net.official@gmail.com>
2022-01-28 18:25:48 +0900

    Improve：ChatRoomBarをfixturesの共通データを使用する形に変更

--- src/components/molecules/ChatRoomBar/stories.tsx
+++ src/components/molecules/ChatRoomBar/stories.tsx
@@ -1,27 +1,12 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import React from "react";
 import { ChatRoomBar } from ".";
-import { IChatRoom } from "@types";
-import { USER } from "./fixture";
-import { Timestamp } from "@/plugin/firebase";
+import { CHATROOM } from "@fixtures";
 
 export default {
   title: "@components/molecules/ChatRoomBar",
   component: ChatRoomBar,
 } as ComponentMeta<typeof ChatRoomBar>;
-
-const CHATROOM: IChatRoom = {
-  id: "this is storybook",
-  owner: USER,
-  name: "storybook",
-  iconURL: "/logo.png",
-  tags: [
-    { id: "test-tag1-id", value: "test-tag1-value" },
-    { id: "test-tag2-id", value: "test-tag2-value" },
-  ],
-  description: "hello! this is chatroom card testing for storybook.",
-  createdAt: Timestamp.fromDate(new Date()),
-};
 
 const Template: ComponentStory<typeof ChatRoomBar> = (args) => (
   <ChatRoomBar {...args} />
@@ -30,5 +15,4 @@ const Template: ComponentStory<typeof ChatRoomBar> = (args) => (
 export const chatRoomBar = Template.bind({});
 chatRoomBar.args = {
   chatRoom: CHATROOM,
-  // user: USER,
 };
--- src/components/molecules/ChatRoomBar/test.tsx
+++ src/components/molecules/ChatRoomBar/test.tsx
@@ -4,26 +4,14 @@ import "jest-styled-components";
 import "jsdom-global/register";
 
 import { ChatRoomBar } from ".";
-import { IMessage } from "@types";
 import { Props } from "./types";
-import { USER } from "./fixture";
+import { CHATROOM } from "@fixtures";
 
 describe("<ChatRoomBar />", () => {
   const props: Props = {
     onClick: jest.fn(),
     onTagClick: jest.fn(),
-    chatRoom: {
-      id: "test",
-      owner: USER,
-      name: "storybook",
-      iconURL: "/favicon.ico",
-      tags: [
-        { id: "test-tag1-id", value: "test-tag1-value" },
-        { id: "test-tag2-id", value: "test-tag2-value" },
-      ],
-      description: "hello! this is chatroom card testing for storybook.",
-      createdAt: { seconds: 0, nanoseconds: 0 } as IMessage["postedAt"],
-    },
+    chatRoom: CHATROOM,
   };
   const chatRoomBar = shallow(<ChatRoomBar {...props} />);
 
