0bc8ad4a1d9aa569cb215f9c936e9cf84af5555a
neighbor.net <neighbor.net.official@gmail.com>
2022-02-01 23:32:55 +0900

    Add：pageで行う処理内容と、必要なimportをコメント

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -2,16 +2,30 @@ import type { NextPage } from "next";
 // import { useAuth } from "@/context/AuthContext";
 import { Layout } from "@components/layout";
 import React from "react";
+import { Profile } from "@components/templates";
+// import {
+//   ListTab,
+//   UserInformation,
+//   ChatRoomBarList,
+//   UserBarList,
+// } from "@/components/organisms";
 
 const Home: NextPage = () => {
   // const { currentUser, login, logout } = useAuth();
+  /**
+   * profile画面でやること
+   * 1. routes.idからuidを持つuserを検索する
+   *     見つからなかった場合、undefined値を代入する
+   * 2. user.belongRoomsからroomの情報を取得する
+   *      ( room取得時、tagsの取得も同時に行う。（tagsはdocRef配列） )
+   * 3. user.followsからフォローリストを全件取得する
+   * 4. user.followersからフォロワーリストを全件取得する
+   *      ( 本番verでは件数制限を行い、「次の十件を取得」ボタンみたいなのを追加する )
+   */
+
   return (
     <Layout title="Profile">
-      <h1>現在この画面は工事中です！( TДT)ｺﾞﾒﾝﾖｰ</h1>
-      <p>Profile画面は、各ユーザーの情報の閲覧が可能なページです。</p>
-      <p>
-        活動記録や、公開メッセージ、所属しているルームリストや、連携しているSNSアカウントのリンク等を表示可能です。
-      </p>
+      {/* <Profile listTabArea={} userInformationArea={} /> */}
     </Layout>
   );
 };
