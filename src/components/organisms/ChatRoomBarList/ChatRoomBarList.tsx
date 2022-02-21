2ab100669bf0b31f081a931a0c472d7a12668283
neighbor.net <neighbor.net.official@gmail.com>
2022-02-22 01:23:02 +0900

    add:ui id

--- src/components/organisms/ChatRoomBarList/ChatRoomBarList.tsx
+++ src/components/organisms/ChatRoomBarList/ChatRoomBarList.tsx
@@ -8,13 +8,14 @@ export const ChatRoomBarList = ({ ...props }: Props) => {
   return (
     <Stack direction="column" spacing={2}>
       {props.chatRooms.map((chatRoom: IChatRoom, i: number) => (
-        <ChatRoomBar
-          key={i}
-          chatRoom={chatRoom}
-          onClick={props.onOpenClick}
-          onUserClick={props.onUserClick}
-          onTagClick={props.onTagClick}
-        />
+        <span key={i} id={`chat_room_bar_list-chat_room_bar_${chatRoom.id}`}>
+          <ChatRoomBar
+            chatRoom={chatRoom}
+            onClick={props.onOpenClick}
+            onUserClick={props.onUserClick}
+            onTagClick={props.onTagClick}
+          />
+        </span>
       ))}
     </Stack>
   );
