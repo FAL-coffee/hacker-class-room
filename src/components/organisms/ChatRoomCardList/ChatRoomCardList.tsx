3451ab5943bebf5f9c6470f05e891ef0a34b3edd
FAL-coffee <fal.engineer.2001@gmail.com>
2022-02-23 08:08:34 +0000

    add:testfile

--- src/components/organisms/ChatRoomCardList/ChatRoomCardList.tsx
+++ src/components/organisms/ChatRoomCardList/ChatRoomCardList.tsx
@@ -8,13 +8,14 @@ export const ChatRoomCardList = ({ ...props }: Props) => {
   return (
     <Stack direction="column" spacing={3}>
       {props.chatRooms.map((chatRoom: IChatRoom, i: number) => (
-        <ChatRoomCard
-          key={i}
-          chatRoom={chatRoom}
-          onOpenClick={props.onOpenClick}
-          onTagClick={props.onTagClick}
-          onUserClick={props.onUserClick}
-        />
+        <span key={i} id={`chat_room_card_list-chat_room_card_${chatRoom.id}`}>
+          <ChatRoomCard
+            chatRoom={chatRoom}
+            onOpenClick={props.onOpenClick}
+            onTagClick={props.onTagClick}
+            onUserClick={props.onUserClick}
+          />
+        </span>
       ))}
     </Stack>
   );
--- src/components/organisms/ChatRoomCardList/test.tsx
+++ src/components/organisms/ChatRoomCardList/test.tsx
@@ -3,20 +3,57 @@ import { shallow } from "enzyme";
 import "jest-styled-components";
 import "jsdom-global/register";
 
+import { ChatRoomCard } from "@/components/molecules";
 import { ChatRoomCardList } from ".";
 import { Props } from "./types";
 import { CHATROOM } from "@fixtures";
+import { IChatRoom } from "@/types";
+
+const getChatRoomDemoDatas = (): IChatRoom[] => {
+  let chatRoomDemoDatas: IChatRoom[] = [];
+  for (let i = 0; i < 5; i++) {
+    chatRoomDemoDatas.push({ ...CHATROOM, id: CHATROOM.id + i });
+  }
+  return chatRoomDemoDatas;
+};
 
 describe("<ChatRoomCardList />", () => {
   const props: Props = {
     onOpenClick: jest.fn(),
     onTagClick: jest.fn(),
     onUserClick: jest.fn(),
-    chatRooms: [CHATROOM, CHATROOM],
+    chatRooms: getChatRoomDemoDatas(),
   };
   const chatRoomCardList = shallow(<ChatRoomCardList {...props} />);
+  it("All chatroomcards will receive their props correctly.", () => {
+    props.chatRooms.forEach((chatRoom: IChatRoom, i: number) => {
+      expect(
+        chatRoomCardList
+          .find(`#chat_room_card_list-chat_room_card_${chatRoom.id}`)
+          .find(ChatRoomCard)
+          .props().chatRoom
+      ).toEqual(chatRoom);
 
-  it("render", () => {
-    expect(chatRoomCardList.exists());
+      expect(
+        chatRoomCardList
+          .find(`#chat_room_card_list-chat_room_card_${chatRoom.id}`)
+          .find(ChatRoomCard)
+          .props().onOpenClick
+      ).toEqual(props.onOpenClick);
+
+      expect(
+        chatRoomCardList
+          .find(`#chat_room_card_list-chat_room_card_${chatRoom.id}`)
+          .find(ChatRoomCard)
+          .props().onUserClick
+      ).toEqual(props.onUserClick);
+
+      expect(
+        chatRoomCardList
+          .find(`#chat_room_card_list-chat_room_card_${chatRoom.id}`)
+          .find(ChatRoomCard)
+          .props().onTagClick
+      ).toEqual(props.onTagClick);
+    });
   });
 });
