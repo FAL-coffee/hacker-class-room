2427a487a763a5ffd69a4959223162d7fb5f363b
neighbor.net <neighbor.net.official@gmail.com>
2022-03-02 23:46:36 +0900

    remove:Argument genreId of onTagClick in chatroombar types

--- src/components/molecules/ChatRoomBar/types.ts
+++ src/components/molecules/ChatRoomBar/types.ts
@@ -2,6 +2,6 @@ import { IChatRoom } from "@types";
 export interface Props {
   chatRoom: IChatRoom;
   onClick: (id: string) => void;
-  onUserClick:(uid:string)=>void;
-  onTagClick: (genreId:string,id: string) => void;
+  onUserClick: (uid: string) => void;
+  onTagClick: (id: string) => void;
 }
