107d97b002ea85b48911b3a16a8063e7bc416800
neighbor.net <neighbor.net.official@gmail.com>
2022-03-06 23:53:02 +0900

    add:promise all option

--- src/hooks/useBelongRooms.ts
+++ src/hooks/useBelongRooms.ts
@@ -27,52 +27,57 @@ export const useBelongRooms = (): //   initialState?: DocumentSnapshot
   // const [tempBelongRooms, setTempBelongRooms] = useState<IChatRoom[]>([]);
   const [userSnap, setUserSnap] = useState<DocumentSnapshot>();
   const [belongRooms, setBelongRooms] = useState<IChatRoom[]>([]);
-  const [loading, setLoading] = useState<Boolean>(true);
+  const [loading, setLoading] = useState<Boolean>(false);
   useEffect(() => {
-    if (!userSnap) return;
-    const userSnapData = userSnap.data() as IUser;
-    if (!userSnapData?.belongRooms) return;
-    else if (!!userSnapData?.belongRooms) {
-      const belongRoomDatas: IChatRoom[] = [];
-      userSnapData.belongRooms.map(
-        async (belongRoomRef: DocumentReference, i: number) => {
-          const belongRoomDoc = await getDoc(belongRoomRef);
-          const tempBelongRoomData = belongRoomDoc.data() as ITempChatRoom;
-          const belongRoomData: IChatRoom = {
-            ...tempBelongRoomData,
-            id: belongRoomRef.id,
-            tags: [],
-            owner: {
-              displayName: "user is not found",
-              email: "user is not found",
-              uid: "404",
-            },
-          };
-          // tagsの取得・格納
-          // if (!!tempBelongRoomData.tags) {
-          Object.values(tempBelongRoomData.tags).map(
-            async (tag: DocumentReference, j: number) => {
-              const tagDoc = await getDoc(tag);
-              const tagData = await tagDoc.data();
-              belongRoomData.tags.push({
-                id: tag.id,
-                value: tagData?.value,
-              });
+    (async () => {
+      await setLoading(true);
+      if (!userSnap) return;
+      const userSnapData = userSnap.data() as IUser;
+      if (!userSnapData?.belongRooms) return;
+      else if (!!userSnapData?.belongRooms) {
+        const belongRoomDatas: IChatRoom[] = [];
+        await Promise.all(
+          userSnapData.belongRooms.map(
+            async (belongRoomRef: DocumentReference, i: number) => {
+              const belongRoomDoc = await getDoc(belongRoomRef);
+              const tempBelongRoomData = belongRoomDoc.data() as ITempChatRoom;
+              const belongRoomData: IChatRoom = {
+                ...tempBelongRoomData,
+                id: belongRoomRef.id,
+                tags: [],
+                owner: {
+                  displayName: "user is not found",
+                  email: "user is not found",
+                  uid: "404",
+                },
+              };
+              // tagsの取得・格納
+              // if (!!tempBelongRoomData.tags) {
+              await Object.values(tempBelongRoomData.tags).map(
+                async (tag: DocumentReference, j: number) => {
+                  const tagDoc = await getDoc(tag);
+                  const tagData = await tagDoc.data();
+                  belongRoomData.tags.push({
+                    id: tag.id,
+                    value: tagData?.value,
+                  });
+                  console.log(belongRoomData.tags);
+                }
+              );
+
+              const ownerRef = tempBelongRoomData.owner;
+              const ownerDoc = await getDoc(ownerRef);
+              const ownerData = ownerDoc.data();
+              belongRoomData.owner = ownerData as IUser;
+
+              await belongRoomDatas.push(belongRoomData);
+              setBelongRooms([...belongRoomDatas]);
             }
-          );
-          // }
-
-          const ownerRef = tempBelongRoomData.owner;
-          const ownerDoc = await getDoc(ownerRef);
-          const ownerData = ownerDoc.data();
-          belongRoomData.owner = ownerData as IUser;
-
-          await belongRoomDatas.push(belongRoomData);
-          setBelongRooms([...belongRoomDatas]);
-        }
-      );
-    }
-    setLoading(false);
+          )
+        );
+      }
+      await setLoading(false);
+    })();
   }, [userSnap]);
 
   return [belongRooms, loading, setUserSnap];
