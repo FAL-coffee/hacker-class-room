8b7db604251972fb219052cb3421ef67fe6a2b4f
neighbor.net <neighbor.net.official@gmail.com>
2022-03-22 01:54:48 +0900

    Revert "fix:call func check"
    This reverts commit 16f807d0e6eb9dee949513e96e82730fa206d24c.

--- src/utils/firebase/converter.ts
+++ src/utils/firebase/converter.ts
@@ -17,8 +17,7 @@ export const converter = <
       // Date型の値をTimestamp型に変換する
       if (
         typeof data[key].toString == "function" &&
-        typeof data[key].toString().call == "function" &&
-        data[key].toString().call(new Date())
+        data[key].toString.call(new Date())
       ) {
         (data as DocumentSnapshotType)[key] = Timestamp.fromDate(data[key]);
       }
@@ -31,7 +30,6 @@ export const converter = <
       // Timestamp型の値をDate型に変換する
       if (
         typeof data[key].toString == "function" &&
-        typeof data[key].toString.startsWith == "function" &&
         data[key].toString().startsWith("Timestamp")
       ) {
         (data as DocumentSnapshotType)[key] = data[key].toDate();
