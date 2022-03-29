1ffcd49cd1c6b811010b4af96fc71047210e1f2e
neighbor.net <neighbor.net.official@gmail.com>
2022-03-29 18:33:49 +0900

    add:FollowButton, DirectMessageIconButtons props types

--- src/components/organisms/UserInformation/types.ts
+++ src/components/organisms/UserInformation/types.ts
@@ -3,8 +3,18 @@ export interface Props {
   user: IUser;
   isMe: boolean;
   following?: boolean;
-  freezeDirectMessage?:boolean;
+  freezeDirectMessage?: boolean;
   onFollowClick?: (uid: string) => void;
   onUnFollowClick?: (uid: string) => void;
   onSendMessageClick?: (uid: string) => void;
+}
+
+export interface FollowButtonProps {
+  following: boolean;
+  onClick: () => void;
+}
+
+export interface DirectMessageIconButtonProps {
+  disabled: boolean;
+  onClick: () => void;
 }
